51fc688833979636d5bdcb9f4e2bbc274b7fa9e4
ekothekoch <ekothekoch@gmail.com>
2020-02-13 15:31:36 +0300

    Add files via upload
    Scripts are added

---
+++ GameManager.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public enum GamePhases{Player1,Player2,Player3,Player4}
+public  class GameManager : MonoBehaviour
+{
+    public GamePhases gamePhase;
+    public  Player[] players;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamePhase = GamePhases.Player1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisableOthers();
+        //FindPhase();
+      
+    }
+    void DisableOthers()
+    {
+        if (gamePhase == GamePhases.Player1)
+        {
+            
+            for(int x=0; x<players.Length;x++)
+            {
+                if (players[x].isim == "1")
+                {
+                    players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 1;
+                    continue;
+                }
+                
+                    players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 0;
+                 
+                
+                for (int y=0; y<players[x].buttons.Length;y++)
+                {
+
+                    players[x].buttons[y].interactable = false;
+                }
+            }
+
+        players[3].played = false;
+
+        FindPhase();
+        //Debug.Log(gamePhase);
+    }
+        if (gamePhase == GamePhases.Player2)
+        {
+            
+            for (int x = 0; x < players.Length; x++)
+            {
+                if (players[x].isim == "2")
+                {
+                    players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 1;
+                    continue;
+                }
+                   
+                players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 0;
+
+                for (int y = 0; y < players[x].buttons.Length; y++)
+                    {
+                        players[x].buttons[y].interactable = false;
+                    }
+                
+            }
+            players[0].played = false;
+
+            FindPhase();
+           // Debug.Log(gamePhase);
+
+
+        }
+         if (gamePhase == GamePhases.Player3)
+        {
+            for (int x = 0; x < players.Length; x++)
+            {
+                if (players[x].isim == "3")
+                {
+                    players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 1;
+                    continue;
+                   
+                }
+                players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 0;
+                for (int y = 0; y < players[x].buttons.Length; y++)
+                {
+                    players[x].buttons[y].interactable = false;
+                }
+
+            }
+            players[1].played = false;
+            FindPhase();
+           // Debug.Log(gamePhase);
+        }
+        if (gamePhase == GamePhases.Player4)
+        {
+            for (int x = 0; x < players.Length; x++)
+            {
+                if (players[x].isim == "4")
+                {
+                    players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 1;
+                    continue;
+                }
+                players[x].gameObject.GetComponentInChildren<CanvasGroup>().alpha = 0;
+                for (int y = 0; y < players[x].buttons.Length; y++)
+                {
+                    players[x].buttons[y].interactable = false;
+                }
+
+            }
+
+            players[2].played = false;
+            FindPhase();
+            //Debug.Log(gamePhase);
+        }
+    }
+    void FindPhase()
+    {
+        if (gamePhase == GamePhases.Player1)
+        {
+            if (players[0].played)
+            {
+                for (int y = 0; y < players[1].buttons.Length; y++)
+                    players[1].buttons[y].interactable = true;
+
+                gamePhase = GamePhases.Player2;
+            }
+        }
+
+        if (gamePhase == GamePhases.Player2)
+        { if(players[1].played)
+                {
+                for (int y = 0; y < players[2].buttons.Length; y++)
+                    players[2].buttons[y].interactable = true;
+
+                gamePhase = GamePhases.Player3;
+            }
+        }
+        if (gamePhase == GamePhases.Player3)
+        {
+            if (players[2].played)
+            {
+                for (int y = 0; y < players[3].buttons.Length; y++)
+                    players[3].buttons[y].interactable = true;
+                gamePhase = GamePhases.Player4;
+            }
+        }
+        if (gamePhase == GamePhases.Player4)
+        {
+            if (players[3].played)
+            {
+                for (int y = 0; y < players[0].buttons.Length; y++)
+                    players[0].buttons[y].interactable = true;
+
+                gamePhase = GamePhases.Player1;
+               
+            }
+        }
+    }
+    }
+
---
+++ Player.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player :MonoBehaviour
+{
+    public string isim;
+    public Text populationText;
+    int result;
+    GameObject target;
+    public GameObject[] enemies;
+    public Button[] buttons;
+    public Image DefendImage, AttackImage, PropagandaImage;
+    public Sprite radar;
+    public Sprite s400;
+    public Sprite fighters;
+    public Sprite nuclear;
+    public Sprite propaganda;
+    [Range(20000000, 100000000)]
+    public int population = 50000000/10000;
+    public bool played;
+    private void Start()
+    {
+        buttons[1].enabled = false;
+        buttons[2].enabled = false;
+        buttons[0].enabled = false;
+        populationText.text = population.ToString();
+    }
+   
+    public void ChangeRadarImage()
+    {
+     
+        DefendImage.sprite = radar;
+        DisableDropDownList();
+        played = true;
+    }
+    public void ChangeS400Image()
+    {
+        DefendImage.sprite = s400;
+        DisableDropDownList();
+        played = true;
+    }
+    public void PlayerSelected()
+    {
+        result = gameObject.GetComponentInChildren<Dropdown>().value;
+        buttons[1].enabled = true;
+        buttons[2].enabled = true;
+        buttons[0].enabled = true;
+        Debug.Log(result);
+    }
+    
+    public void ChangeFighterImage()
+    {
+        AttackImage.sprite = fighters;
+        DisableDropDownList();
+        played = true;
+    }
+    public void ChangeNuclearImage()
+    {
+        for (int x = 0; x < enemies.Length; x++)
+        {
+            if (result == int.Parse(enemies[x].GetComponent<Player>().isim))
+            {
+                target = enemies[x];
+                Debug.Log(target);
+                
+            }
+        }
+        NuclearResult(target);
+        UpdatePopulation();
+        AttackImage.sprite = nuclear;
+        DisableDropDownList();
+        played = true;
+    }
+    void UpdatePopulation()
+    {
+        target.GetComponent<Player>().populationText.text = target.GetComponent<Player>().population.ToString();
+    }
+    public void ChangePropagandaImage()
+    {
+        PropagandaImage.sprite = propaganda;
+        DisableDropDownList();
+        played = true;
+    }
+    public void DisableDropDownList()
+    {
+        gameObject.GetComponentInChildren<CanvasGroup>().alpha = 0;
+    }
+    #region RESULTS
+    public void NuclearResult(GameObject newTarget)
+    {
+        //target=newTarget;
+        float a = Random.Range(0f, 1f);
+        if(a>=0.5f)
+        {
+            newTarget.GetComponent<Player>().population -= 3000000;
+        }
+        else if(a>=.2f && a<.5f)
+        {
+            Debug.Log("You missed");
+        }
+        else if (a<.2f)
+        {
+            Debug.Log("BackFired");
+        }
+        Debug.Log(newTarget.GetComponent<Player>().population);
+    }
+    #endregion RESULTS
+}
